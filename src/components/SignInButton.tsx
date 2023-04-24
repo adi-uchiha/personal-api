622ff6634268692002773727d296fdc7f87a9992
adi-uchiha <adi.cs.night@gmail.com>
2023-04-24 05:46:09 +0530

    Add google icon svg

--- src/components/SignInButton.tsx
+++ src/components/SignInButton.tsx
@@ -2,6 +2,7 @@
 
 import { signIn } from "next-auth/react"
 import { FC, useState } from "react"
+import GoogleSvg from "./GoogleSvg"
 import Button from "./ui/Button"
 import { toast } from "./ui/Toast"
 
@@ -11,7 +12,7 @@ const SignInButton: FC<SignInButtonProps> = ({}) => {
   const [isLoading, setIsLoading] = useState<boolean>(false)
 
   const signInWithGoogle = async () => {
-    // setIsLoading(true)
+    setIsLoading(true)
     try {
       await signIn('google')
     } catch (error) {
@@ -24,7 +25,9 @@ const SignInButton: FC<SignInButtonProps> = ({}) => {
     }
   }
 
-  return <Button onClick={signInWithGoogle} isLoading={isLoading}>Sign In</Button>
+  return <Button onClick={signInWithGoogle} isLoading={isLoading}>
+    {isLoading ? null : (<GoogleSvg />)}
+    Sign In</Button>
 }
 
 export default SignInButton
